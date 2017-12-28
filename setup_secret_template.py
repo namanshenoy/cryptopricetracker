4b0c66e9f3c186d5154a5711c7f0bdbe519491a1
namanshenoy <naman.skillet@gmail.com>
2017-12-28 00:58:32 -0500

    Moved portfolio to settings

--- setup_secret_template.py
+++ setup_secret_template.py
@@ -9,7 +9,25 @@ SETTINGS = {
     'AUTH_TOKEN' : 'XXXXXXXXX-TEMPLATE',
     'SEND_TO_PHONE': '+1XXXXXXXX-TEMPLATE',
     'SEND_FROM_PHONE': '+1XXXXXXXX-TEMPLATE',
-    'SET_ALERT': -1
+    'SET_ALERT': -1,
+    'NEXT_ALERT': 10
+}
+
+PORTFOLIO = {
+        "brdbtc":{
+            "name":"BRD",
+            "conv":"BTC",
+            "quan":100,
+            "code":"brdbtc",
+            "notify":True
+        },
+        "xvgbtc":{
+            "name":"XVG",
+            "conv":"BTC",
+            "quan":1000,
+            "code":"xvgbtc",
+            "notify":False
+        }
 }
 
 
--- websockets.py
+++ websockets.py
@@ -114,50 +114,15 @@ if __name__ == '__main__':
 
     setup_secret.run_settings()
     sms_client = send_sms.TwilioClient()
+    ALERT_PRICE = float(os.environ['SET_ALERT'])
+    ALERT_UPDATE_VAL = float(os.environ['NEXT_ALERT'])
 
     #sms_client.send_sms("This shit worked!")
     
     get_btc_usd()
     threading.Timer(10.0, get_btc_usd).start()
 
-    my_coins = {
-        "brdbtc":{
-            "name":"BRD",
-            "conv":"BTC",
-            "quan":51.948,
-            "code":"brdbtc"
-        },
-        "xvgbtc":{
-            "name":"XVG",
-            "conv":"BTC",
-            "quan":466.794,
-            "code":"xvgbtc"
-        },
-        "adabtc":{
-            "name":"ADA",
-            "conv":"BTC",
-            "quan":50.949,
-            "code":"adabtc"
-        },
-        "neobtc":{
-            "name":"NEO",
-            "conv":"BTC",
-            "quan":0.789865,
-            "code":"neobtc"
-        },
-        "xrpbtc":{
-            "name":"XRP",
-            "conv":"BTC",
-            "quan":73.926,
-            "code":"xrpbtc"
-        },
-        "iotabtc":{
-            "name":"IOTA",
-            "conv":"BTC",
-            "quan":6.993,
-            "code":"iotabtc"
-        }
-    }
+    my_coins = setup_secret.PORTFOLIO
 
 
     try:
@@ -174,21 +139,25 @@ if __name__ == '__main__':
                 coin, btc_val = ws.get_btc_value()
                 coin_queue.values[coin] = btc_val
                 if not ws.terminated:
-                    #coin_values[coin] = btc_val 
                     break
                 else:
                     break
+
             os.system("clear")
-            #print(coin_queue.values)
+
             total_val = 0.
             total_btc = 0.
             for coin in coin_queue.values:
                 current_val = float(coin_queue.values[coin])* BTC_USD * my_coins[coin]['quan']
-                total_val+= current_val
-                total_btc+= float(coin_queue.values[coin]) * my_coins[coin]['quan']
+                if my_coins[coin]['notify']:
+                    total_val+= current_val
+                    total_btc+= float(coin_queue.values[coin]) * my_coins[coin]['quan']
                 print("{0} - {1} USD: {2:.2f}\t|\tSingle: {3:.4f}".format(coin.upper()[:-3], "BTC", current_val, coin_queue.values[coin]*BTC_USD))
             print ("Total Binance Value : {0:.2f}".format(total_val))
             print("Total Value in Bitcoin: {0:.8f}\n(USD/BTC from Coinbase)".format(total_btc))
+            if ALERT_PRICE is not -1 and total_val >= ALERT_PRICE:
+                sms_client.send_sms("Your portfolio has reached {0}.\nNext text notification set to: {1}".format(ALERT_PRICE, ALERT_UPDATE_VAL+ALERT_PRICE))
+                ALERT_PRICE += ALERT_UPDATE_VAL
             time.sleep(3)
 
     except KeyboardInterrupt:
