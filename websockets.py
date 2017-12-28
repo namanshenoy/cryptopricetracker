454378ff49698ea9b17a3d459fe789c65960dd48
namanshenoy <naman.skillet@gmail.com>
2017-12-27 19:43:17 -0500

    Linter sucks... Changed some stuff to fix a few linter warnings

--- websockets.py
+++ websockets.py
@@ -1,12 +1,12 @@
+import json
 from ws4py.client import WebSocketBaseClient
 from ws4py.manager import WebSocketManager
 from ws4py import format_addresses, configure_logger
-import json
 import requests
 
-logger = configure_logger()
+LOGGER = configure_logger()
 
-manager = WebSocketManager()
+MANAGER = WebSocketManager()
 
 #FIXME: Change this global to local variable
 BTC_USD = 0.0
@@ -19,26 +19,26 @@ def get_btc_usd():
     try:
         global BTC_USD
         BTC_USD = float(response.json()['data']['amount'])
-        logger.info("Updated BTC Value: {}".format(BTC_USD))
+        LOGGER.info("Updated BTC Value: %f" % float(BTC_USD))
     except Exception as e:
-        logger.warning("BTC Value could not updated \n{}".format(str(e)))
+        LOGGER.warning("BTC Value could not updated \n{}".format(str(e)))
         pass
 
 
 class CryptoClient(WebSocketBaseClient):
     """
     Custom client for websockets.
-        :param WebSocketBaseClient: 
+        :param WebSocketBaseClient: Superclas for WebSocketClient from ws4py
     """
 
     def set_info(self, from_crypto, to_crypto, code=None):
         """
         Set From Conversion and To conversion cryptos, set universal conversion code
-            :param self: 
-            :param from_crypto: 
-            :param to_crypto:
-            :param code: 
-        """   
+            :param self:.
+            :param from_crypto: Cryto ticker from which to convert
+            :param to_crypto: Cryto ticker to which to convert
+            :param code: Unique identifier for this conversion
+        """ 
         self.from_crypto = from_crypto
         self.to_crypto = to_crypto
         if code is not None:
@@ -47,8 +47,8 @@ class CryptoClient(WebSocketBaseClient):
             self.code = from_crypto.lower()+to_crypto.lower()
 
     def handshake_ok(self):
-        logger.info("Opening %s" % format_addresses(self))
-        manager.add(self)
+        LOGGER.info("Opening %s" % format_addresses(self))
+        MANAGER.add(self)
 
     def received_message(self, msg):
         """
@@ -67,17 +67,17 @@ if __name__ == '__main__':
     threading.Timer(10.0, get_btc_usd).start()
 
     try:
-        manager.start() 
-        client = CryptoClient('wss://stream.binance.com:9443/ws/ethbtc@kline_1m')
-        client.set_info(from_crypto='ETH', to_crypto='BTC')
-        client.connect()
+        MANAGER.start()
+        CLIENT = CryptoClient('wss://stream.binance.com:9443/ws/ethbtc@kline_1m')
+        CLIENT.set_info(from_crypto='ETH', to_crypto='BTC')
+        CLIENT.connect()
 
-        client = CryptoClient('wss://stream.binance.com:9443/ws/brdbtc@kline_1m')
-        client.set_info(from_crypto='BRD', to_crypto='BTC', code='brdbtc')
-        client.connect()
+        CLIENT = CryptoClient('wss://stream.binance.com:9443/ws/brdbtc@kline_1m')
+        CLIENT.set_info(from_crypto='BRD', to_crypto='BTC', code='brdbtc')
+        CLIENT.connect()
 
         while True:
-            for ws in manager.websockets.values():
+            for ws in MANAGER.websockets.values():
                 if not ws.terminated:
                     break
             else:
@@ -85,7 +85,6 @@ if __name__ == '__main__':
             print("NEXT SET")
             time.sleep(3)
     except KeyboardInterrupt:
-        manager.close_all()
-        manager.stop()
-        manager.join()
-
+        MANAGER.close_all()
+        MANAGER.stop()
+        MANAGER.join()
